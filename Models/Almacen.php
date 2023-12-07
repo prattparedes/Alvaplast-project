f225da6f187d32682bd248bf99158d3793635da5
fabianquispe12 <fabianquispe241@gmail.com>
2023-12-07 17:25:50 -0500

    tabla Compra

--- Models/Almacen.php
+++ Models/Almacen.php
@@ -1,8 +1,8 @@
 <?php
-require_once("../config/connection.php");
+require_once("../connection.phpconnection.php");
 
 class Almacen{
-
+    
     public static function getAlmacenes(){
         $con = Connection::Conectar();
         $data=$con->query("exec sp_ListarAlmacen");
