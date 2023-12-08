64efce13a8384d686ba56fb518a62a1b0794f791
fabianquispe12 <fabianquispe241@gmail.com>
2023-12-07 19:11:57 -0500

    pequeño cambio

--- views/modals/sellorderlist.php
+++ views/modals/sellorderlist.php
@@ -8,7 +8,6 @@
     <input type="radio" id="no_facturable" name="facturabilidad" value="no_facturable">
 </div>
 <hr>
-<<<<<<< HEAD
 <table class="table">
     <thead>
         <tr>
@@ -39,40 +38,3 @@
        <?php }?>
     </tbody>
 </table>
-=======
-<div class="modal__table--container">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Cliente</th>
-                <th>Orden</th>
-                <th>Fecha Emisión</th>
-                <th>Tipo de Pago</th>
-                <th>Importe</th>
-                <th>Moneda</th>
-                <th>Vendedor</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Nombre Cliente 1</td>
-                <td>Orden 123</td>
-                <td>2023-12-01</td>
-                <td>Efectivo</td>
-                <td>250.00</td>
-                <td>Dólares</td>
-                <td>Nombre del Vendedor</td>
-            </tr>
-            <tr>
-                <td>Nombre Cliente 2</td>
-                <td>Orden 456</td>
-                <td>2023-12-03</td>
-                <td>Tarjeta de Crédito</td>
-                <td>150.00</td>
-                <td>Euros</td>
-                <td>Otro Nombre de Vendedor</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
->>>>>>> bc9536472ed431e1ff6a801ece14bbeb4e7c37f0
