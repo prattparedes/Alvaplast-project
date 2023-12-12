369d26861fe552777fc50902dc6bb12ee5644a53
Yashira <141366116+Yash1r4@users.noreply.github.com>
2023-12-12 01:34:42 -0500

    Update Venta.php

--- Models/Venta.php
+++ Models/Venta.php
@@ -15,43 +15,21 @@ class Venta {
         return $ventas;
     }
 
-    // Método estático para mostrar las ventas en una tabla HTML.
-    public static function mostrarVentasEnTabla() {
-        // Se obtienen las ventas utilizando el método getVentas.
-        $ventas = self::getVentas();
+     // Método estático para mostrar las ventas sin etiquetas de tabla.
+     public static function mostrarVentas() {
+        // Se obtiene una venta utilizando el método getVentas.
+        $venta = self::getVentas();
 
-        // Se inicia un bucle foreach para recorrer cada venta obtenida.
-        foreach ($ventas as $ven) {
-            ?>
-            <!-- Se inicia una fila de la tabla HTML para mostrar los detalles de cada venta -->
-            <tr>
-                <!-- Se crea una celda para mostrar la razón social de la venta -->
-                <td><?= $ven->razon_social ?></td>
-
-                <!-- Se crea una celda para mostrar el número y serie del documento de la venta -->
-                <td><?= 'OV/'.$ven->numero_documento.'-'.$ven->serie_documento ?></td>
-
-                <!-- Se crea una celda para mostrar la fecha de emisión de la venta (se utiliza explode para obtener solo la fecha) -->
-                <td><?= explode(' ', $ven->fecha_emision)[0] ?></td>
-
-                <!-- Se crea una celda para mostrar el tipo de pago (Efectivo o Crédito) -->
-                <td><?= ($ven->tipo_pago == "E") ? "Efectivo" : "Credito" ?></td>
-
-                <!-- Se crea una celda para mostrar el total de la venta -->
-                <td><?= $ven->total ?></td>
-
-                <!-- Se crea una celda para mostrar la moneda de la venta -->
-                <td><?= $ven->Moneda ?></td>
-
-                <!-- Se crea una celda para mostrar el nombre completo del cliente asociado a la venta -->
-                <td><?= $ven->nombres . ' ' . $ven->ap_paterno . ' ' . $ven->ap_materno ?></td>
-            <!-- Se cierra la fila de la tabla HTML -->
-            </tr>
-            <?php
+        if ($venta) {
+            // Imprime los valores directamente.
+            echo $venta->razon_social . '|';
+            echo 'OV/' . $venta->numero_documento . '-' . $venta->serie_documento . '|';
+            echo explode(' ', $venta->fecha_emision)[0] . '|';
+            echo ($venta->tipo_pago == "E") ? "Efectivo" : "Credito" . '|';
+            echo $venta->total . '|';
+            echo $venta->Moneda . '|';
+            echo $venta->nombres . ' ' . $venta->ap_paterno . ' ' . $venta->ap_materno;
         }
     }
 }
-?>
-
-
 ?>
