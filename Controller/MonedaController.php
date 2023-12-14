012f03ca14e69ca520aa20c04f3f7ec91094283f
fabianquispe12 <fabianquispe241@gmail.com>
2023-12-14 09:41:59 -0500

    pequeño cambio en el controllerMoneda

--- Controller/MonedaController.php
+++ Controller/MonedaController.php
@@ -1,22 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT']."/Alvaplast-project/Models/Moneda.php");
     
-class MonedaController{
-
-    public static function grabarMoneda(){
-        try{
-            $id = 1;
-            $descripcion = $_POST["descripcion"];
-            $abr = $_POST["abreviatura"];
-            Moneda::RegistrarMoneda($id,$descripcion, $abr);
-            
-        }catch(Exception $e){
-            echo  $e->getMessage() ;
-        }
-    }
-
-    }
-
 if(isset($_POST)){
     $id = 1;
     $descripcion = $_POST["descripcion"];
