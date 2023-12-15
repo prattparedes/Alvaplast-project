887244ce401c1a558101d678aea39f1a2731f801
Yashira <141366116+Yash1r4@users.noreply.github.com>
2023-12-14 20:18:26 -0500

    Lista de Proveedor

---
+++ Models/Proveedor.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/Alvaplast-project/config/connection.php');
+
+class Proveedor {
+    // Método estático para obtener todos los proveedores.
+    public static function getProveedores() {
+        // Se establece la conexión utilizando la clase Connection.
+        $connection = Connection::Conectar();
+        
+        // Se ejecuta un procedimiento almacenado para obtener la lista de proveedores.
+        $data = $connection->query("exec sp_ListarProveedor");
+        
+        // Se recuperan los resultados en formato de objeto y se retornan.
+        $proveedores = $data->fetchAll(PDO::FETCH_OBJ);
+        return $proveedores;
+    }
+
+    // Otros métodos para operaciones relacionadas con proveedores pueden ser agregados según sea necesario.
+}
+?>
