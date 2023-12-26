313d651e58e5fc06582772da3956155b5ccc3106
fabianquispe12 <fabianquispe241@gmail.com>
2023-12-26 11:48:42 -0500

    Compra y sus metodos

--- Models/Compra.php
+++ Models/Compra.php
@@ -21,12 +21,24 @@ class Compra{
         return $data->fetchAll(PDO::FETCH_OBJ);
     }
 
-    public static function RegistrarCompra(int $idCompra, string $date,float $total,float $subtotal, float $igv, int $idMoneda, string $numeroDocumento,string $serieDocumento, int $idProveedor,int $idAlmacen,string $tipoPago,int $idPersonal) : bool
+    public static function RegistrarCompra(int $idCompra, string $fecha,float $total,float $subtotal, float $igv, int $idMoneda, string $numeroDocumento,string $serieDocumento, int $idProveedor,int $idAlmacen,string $tipoPago,int $idPersonal) : bool
     {
        try{
         $con = Connection::Conectar();
-        $tsmt = $con->prepare('exec sp_RegistrarCompra ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?');
-        $result=$tsmt->execute([$idCompra,$date,$total,$subtotal,$igv,$idMoneda,$numeroDocumento,$serieDocumento,$idProveedor,$idAlmacen,$tipoPago,$idPersonal]);
+        $tsmt = $con->prepare('exec sp_RegistrarCompra :idCompra, :fecha, :total, :subtotal, :igv, :idMoneda, :numeroDocumento, :serieDocumento, :idProveedor, :idAlmacen,:tipoPago, :idPersonal');
+        $tsmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);                                           //paramtro del idCompra 
+        $tsmt->bindParam(":fecha",$fecha,PDO::PARAM_STR);                                                 //parametro de la fecha 
+        $tsmt->bindParam(":total",$total,PDO::PARAM_STR);                                                 //parametro del total
+        $tsmt->bindParam(":subtotal",$subtotal,PDO::PARAM_STR);                                           //parametro del subtotal
+        $tsmt->bindParam(":igv",$igv,PDO::PARAM_STR);                                                     //parametro del IGV
+        $tsmt->bindParam(":idMoneda",$idMoneda,PDO::PARAM_INT);                                           //parametro del codigo de moneda
+        $tsmt->bindParam(":numeroDocumento",$numeroDocumento,PDO::PARAM_STR);                              //parametro del numero de documento
+        $tsmt->bindParam(":serieDocumento",$serieDocumento,PDO::PARAM_STR);                               //parametro de la serie del documento
+        $tsmt->bindParam(":idProveedor",$idProveedor,PDO::PARAM_INT);                                     //parametro del codigo del proveedor
+        $tsmt->bindParam(":idAlmacen",$idAlmacen,PDO::PARAM_INT);                                         //parametro del codigo de almacen
+        $tsmt->bindParam(":tipoPago",$tipoPago,PDO::PARAM_STR);                                           //parametro del tipo de pago
+        $tsmt->bindParam(":idPersonal",$idPersonal,PDO::PARAM_INT);                                       //parametro del codigo del personal
+        $result = $tsmt->execute();                                 //ejecución del procedimiento almacenado
         return $result;
        }catch(Exception $e){
         echo $e->getMessage();
@@ -40,8 +52,8 @@ class Compra{
         try{
             $con = Connection::Conectar();  
             $tsmt = $con->prepare('sp_EliminarCompra :idCompra, :idPersonal');
-            $tsmt->bindParam(":idCompra", $idCompra, PDO::PARAM_INT,32);
-            $tsmt->bindParam(":idPersonal", $idPersonal, PDO::PARAM_INT,32);
+            $tsmt->bindParam(":idCompra", $idCompra, PDO::PARAM_INT);
+            $tsmt->bindParam(":idPersonal", $idPersonal, PDO::PARAM_INT);
             $result=$tsmt->execute();
             return $result;   
         }catch(Exception $er){
@@ -51,19 +63,38 @@ class Compra{
          
     }
 
-    public static function ListarCompraXid(int $id){
+    public static function ListarCompraXid(int $idCompra){
+       try{
         $con =  Connection::Conectar();
-        $stmt = $con->prepare("exec sp_ListaCompraXID ?");
-        $stmt->execute([$id]);
+        $stmt = $con->prepare("exec sp_ListaCompraXID :idCompra");
+        $stmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+        $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
+       }catch(Exception $e){
+        echo $e->getMessage();
+       }
+        
     }
 
     public static function ModificarCompra(int $idCompra, string $fecha, float $total,float $subtotal, float $igv, int $idMoneda,string $numeroDocumento, string $serieDocumento, int $idProveedor,int $idAlmacen, string $tipoPago, int $idPersonal) :bool
     {
         try{
             $con = Connection::Conectar();
-            $stmt = $con->prepare("exec sp_ModificarCompra ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?");
-            $result=$stmt->execute([$idCompra,$fecha,$total,$subtotal,$igv,$idMoneda,$numeroDocumento,$serieDocumento,$idProveedor,$idAlmacen,$tipoPago,$idPersonal]);
+            $stmt = $con->prepare("exec sp_ModificarCompra :idCompra, :fecha, :total, :subtotal, :igv, :idMoneda, :numeroDocumento, :serieDocumento, :idProveedor, :idAlmacen, :tipoPago, :idPersonal");
+            $stmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+            $stmt->bindParam(":fecha",$fecha,PDO::PARAM_STR);
+            $stmt->bindParam(":total",$total,PDO::PARAM_STR);
+            $stmt->bindParam(":subtotal",$subtotal,PDO::PARAM_STR);
+            $stmt->bindParam(":igv",$igv,PDO::PARAM_STR);
+            $stmt->bindParam(":idMoneda",$idMoneda,PDO::PARAM_INT);
+            $stmt->bindParam(":numeroDocumento",$numeroDocumento,PDO::PARAM_STR);
+            $stmt->bindParam(":serieDocumento",$serieDocumento,PDO::PARAM_STR);
+            $stmt->bindParam(":idProveedor",$idProveedor,PDO::PARAM_INT);
+            $stmt->bindParam(":idAlmacen",$idAlmacen,PDO::PARAM_INT);
+            $stmt->bindParam(":tipoPago",$tipoPago,PDO::PARAM_STR);
+            $stmt->bindParam(":idPersonal",$idPersonal,PDO::PARAM_INT);
+            $result = $stmt->execute();
+            //$result=$stmt->execute([$idCompra,$fecha,$total,$subtotal,$igv,$idMoneda,$numeroDocumento,$serieDocumento,$idProveedor,$idAlmacen,$tipoPago,$idPersonal]);
             return $result;
         }catch(Exception $er){
             echo $er->getMessage();
