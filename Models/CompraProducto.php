ff702c386d9fd5a211387f5c0e125e74fdcdfb56
fabianquispe12 <fabianquispe241@gmail.com>
2023-12-27 14:16:55 -0500

    Cambios en el model de CompraProducto

--- Models/CompraProducto.php
+++ Models/CompraProducto.php
@@ -7,8 +7,14 @@ class CompraProducto{
 public static function RegistrarCompraProducto(int $idCompra,int $idProducto,float $cantidad,float $precioCompra,float $descuento,float $subtotal){
     try{
         $con = Connection::Conectar();
-        $tsmt = $con->prepare("exec sp_RegistrarCompra_Producto ?, ?, ?, ?, ?, ?");
-        $result = $tsmt->execute([$idCompra,$idProducto,$cantidad,$precioCompra,$descuento,$subtotal]);
+        $tsmt = $con->prepare("exec sp_RegistrarCompra_Producto :idCompra, :idProducto, :cantidad, :precioCompra, :descuento, :subtotal");
+        $tsmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+        $tsmt->bindParam(":idProducto",$idProducto,PDO::PARAM_INT);
+        $tsmt->bindParam(":cantidad",$cantidad,PDO::PARAM_STR);
+        $tsmt->bindParam(":precioCompra",$precioCompra,PDO::PARAM_STR);
+        $tsmt->bindParam(":descuento",$descuento,PDO::PARAM_STR);
+        $tsmt->bindParam(":subtotal",$subtotal,PDO::PARAM_STR);
+        $result = $tsmt->execute();
         return $result;
     }catch(Exception $e){
         echo $e->getMessage();
@@ -19,8 +25,9 @@ public static function RegistrarCompraProducto(int $idCompra,int $idProducto,flo
 public static function ListarDetalleCompraXid(int $idCompra){
     try{
         $con = Connection::Conectar();
-        $stmt = $con->prepare("exec sp_ListaDetalleCompraXCompra ?");
-        $stmt->execute([$idCompra]);
+        $stmt = $con->prepare("exec sp_ListaDetalleCompraXCompra :idCompra");
+        $stmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+        $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }catch(Exception $e){
         echo $e->getMessage();
@@ -32,8 +39,9 @@ public static function EliminarProductoXcompra(int $idCompra):bool
 {
     try{
         $con = Connection::Conectar();
-        $stmt =$con->prepare("exec sp_EliminarCompra_Producto ?");
-        $result=$stmt->execute([$idCompra]);
+        $stmt =$con->prepare("exec sp_EliminarCompra_Producto :idCompra");
+        $stmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+        $result=$stmt->execute();
         return $result;
     }catch(Exception $e){
         echo $e->getMessage();
@@ -45,8 +53,10 @@ public static function EliminarProductoXProducto(int $idCompra, int $idProducto)
 {
     try{
         $con = Connection::Conectar();
-        $stmt = $con->prepare("exec sp_EliminarCompra_ProductoxProducto ?, ?");
-        $result=$stmt->execute([$idCompra,$idProducto]);
+        $stmt = $con->prepare("exec sp_EliminarCompra_ProductoxProducto :idCompra, :idCompra");
+        $stmt->bindParam(":idCompra",$idCompra,PDO::PARAM_INT);
+        $stmt->bindParam(":idProducto",$idProducto,PDO::PARAM_INT);
+        $result=$stmt->execute();
         return $result;
     }catch(Exception $e){
         echo $e->getMessage();
