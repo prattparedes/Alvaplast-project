84597b6ad010b55b0c5ec2444e9d1a3435f6d767
fabianquispe12 <fabianquispe241@gmail.com>
2024-03-18 01:00:51 -0500

    estado cuenta completo

--- Controller/ventas/EstadoCuentaController.php
+++ Controller/ventas/EstadoCuentaController.php
@@ -1,15 +1,31 @@
 <?php
 
-use Models\ventas\VentaProducto;
-
 require_once($_SERVER['DOCUMENT_ROOT'] . "/Alvaplast-project/autoload.php");
 
 use Models\ventas\EstadoCuenta;
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-    $fechaIni = $_POST['fechaIni'] . ":00";
-    $fechaFin = $_POST['fechaFin'] . ":00";
-    $result = EstadoCuenta::listarEstadoCuenta($fechaIni, $fechaFin);
-    echo json_encode($result);
+    if (isset($_POST["fechaIni"])) {
+        $fechaIni = $_POST['fechaIni'] . ":00";
+        $fechaFin = $_POST['fechaFin'] . ":00";
+        $result = EstadoCuenta::listarEstadoCuenta($fechaIni, $fechaFin);
+        echo json_encode($result);
+    }
+
+    if (isset($_POST["metodo"])) {
+        $idCuenta = (int) $_POST["idCuenta"];
+        $total = (float) $_POST["total"];
+        $pago = (float) $_POST["pago"];
+        if ($_POST["metodo"] === "Anular") {
+            $response = EstadoCuenta::anularPago($idCuenta, $total);
+        } else if ($_POST["metodo"] === "Grabar") {
+            if ($total == $pago) {
+                $response = EstadoCuenta::registrarPagoTotal($idCuenta, $total);
+            } else {
+                $response = EstadoCuenta::registrarPagoParcial($pago, $total, $idCuenta);
+            }
+            $message = ($response) ? "Todo correcto" : "error en insercion ";
+        }
+    }
 }
--- Models/ventas/EstadoCuenta.php
+++ Models/ventas/EstadoCuenta.php
@@ -15,7 +15,7 @@ class EstadoCuenta
         try {
             $con = Connection::Conectar();
             $stmt = $con->prepare("sp_ListarEstadoCuenta :fechaIni,:fechaFin");
-        
+
             $stmt->bindParam(":fechaIni", $fechaIni, PDO::PARAM_STR);
             $stmt->bindParam(":fechaFin", $fechaFin, PDO::PARAM_STR);
             $stmt->execute();
@@ -24,6 +24,51 @@ class EstadoCuenta
         } catch (PDOException $err) {
             echo $err->getMessage();
             return false;
+        }
+    }
+
+
+    public static function registrarPagoParcial(float $pago, float $total, int $idCuenta)
+    {
+        try {
+            $con = Connection::Conectar();
+            $stmt = $con->prepare("exec sp_RegistrarPagoParcial :idCuenta,:monto,:total");
+            $stmt->bindParam(":idCuenta", $idCuenta, PDO::PARAM_INT);
+            $stmt->bindParam(":monto", $pago, PDO::PARAM_STR);
+            $stmt->bindParam(":total", $total, PDO::PARAM_STR);
+            $stmt->execute();
+            return ($stmt->rowCount() > 2) ? true : false;
+        } catch (PDOException $err) {
+            $err->getMessage();
+        }
+    }
+
+    public static function registrarPagoTotal(int $idCuenta, float $total)
+    {
+        try {
+            $con = Connection::Conectar();
+            $stmt = $con->prepare("exec sp_RegistrarPagoTotal :idCuenta, :monto");
+            $stmt->bindParam(":idCuenta", $idCuenta, PDO::PARAM_INT);
+            $stmt->bindParam(":monto", $total, PDO::PARAM_STR);
+            $stmt->execute();
+            return ($stmt->rowCount() > 0);
+        } catch (PDOException $err) {
+            echo $err->getMessage();
+        }
+    }
+
+
+    public static function anularPago(int $idCuenta, float $total)
+    {
+        try {
+            $con = Connection::Conectar();
+            $stmt = $con->prepare("exec sp_AnularPago :idCuenta, :monto");
+            $stmt->bindParam(":idCuenta", $idCuenta, PDO::PARAM_INT);
+            $stmt->bindParam(":monto", $total, PDO::PARAM_STR);
+            $stmt->execute();
+            return ($stmt->rowCount() > 0) ? true : false;
+        } catch (PDOException $err) {
+            echo $err->getMessage();
         }
     }
 }
